6a3655558f94926a0fe62b81227c6d1202b47dd4
yz2698 <yz2698@cornell.edu>
2022-11-21 14:41:32 -0500

    backend code

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 import os
 from db import db
-from db import Course, Assignment, User
+from db import Asset, Park, Comment
 from flask import Flask, request
 import json
 
@@ -14,6 +14,7 @@ app.config["SQLALCHEMY_ECHO"] = True
 
 db.init_app(app)
 with app.app_context():
+    # db.session.query(Comment).delete()
     db.session.commit()
     db.create_all()
 
@@ -22,12 +23,175 @@ with app.app_context():
 def hello():
     return "succcess"
 
+
+
+@app.route("/upload/", methods=["POST"])
+def upload():
+    """
+    endpoint for uploading image to AWS given its base64, then store returning URL
+    """
+    body = json.loads(request.data)
+    image_data = body.get("image_data")
+    if image_data is None:
+        return failure_response("no base64 image passed in!")
+    
+    asset = Asset(image_data=image_data)
+    db.session.add(asset)
+    db.session.commit()
+    return success_response(asset.serialize(), 201)
+
+
+
 @app.route("/parks/")
 def get_parks():
     """
     Endpoint for getting all parks info
     """  
-    pass
+    return success_response({"parks": [t.serialize() for t in Park.query.all()]})
+
+
+
+@app.route("/parks/", methods=["POST"])
+def create_park():
+    """
+    Endpoint for creating a new parking
+    """
+    body = json.loads(request.data)
+    if not body.get("name") or not body.get("address"):
+        return failure_response("didn't provide name or address", 400)
+
+    if Park.query.filter_by(name=body.get("name")).first():
+        return failure_response("This parking already exist", 400)
+
+    new_park = Park(address=body.get("address"), 
+                      name=body.get("name"),
+                      latitude=body.get("latitude",""),
+                      longitude=body.get("longitude",""),
+                      hourlyRate=body.get("hourlyRate",""),
+                      dailyRate=body.get("dailyRate",""),
+                      rateDays=body.get("rateDays",""),
+                      openHours=body.get("openHours",""),
+                      note=body.get("note",""))
+    db.session.add(new_park)
+    db.session.commit()
+    return success_response(new_park.serialize(), 201)
+
+
+
+@app.route("/parks/<int:park_id>/")
+def get_park(park_id):
+    """
+    Endpoint for getting a park by id
+    """
+    park = Park.query.filter_by(id=park_id).first()
+    if not park:
+        return failure_response("park not found")
+    return success_response(park.serialize())
+
+
+
+@app.route("/parks/<int:park_id>/", methods=["DELETE"])
+def delete_park(park_id):
+    """
+    Endpoint for deleting a park by id
+    """
+    park = Park.query.filter_by(id=park_id).first()
+    if not park:
+        return failure_response("park not found")
+    db.session.delete(park)
+    db.session.commit()
+    return success_response(park.serialize())
+
+
+
+# @app.route("/parks/<int:park_id>/", methods=["POST"])
+# def edit_park(park_id):
+#     """
+#     Endpoint for editing a park attribute
+#     """
+#     park = Park.query.filter_by(id=park_id).first()
+#     if not park:
+#         return failure_response("park not found")
+
+#     body = json.loads(request.data)
+
+#     new_park = Park(
+#         netid=body.get("netid"),
+#         park=body.get("park"),
+#         park_id=park_id
+#     )
+#     db.session.add(new_park)
+#     db.session.commit()
+#     return success_response(new_park.serialize(),201)
+
+
+
+@app.route("/parks/<int:park_id>/comment/", methods=["POST"])
+def create_comment(park_id):
+    """
+    Endpoint for creating a comment for a park by id
+    """
+    park = Park.query.filter_by(id=park_id).first()
+    if not park:
+        return failure_response("park not found")
+
+    body = json.loads(request.data)
+    if not body.get("netid") or not body.get("comment"):
+        return failure_response("didn't provide info", 400)
+
+    image_data = body.get("image_data")
+    img_url = ""
+    if image_data:
+        # upload image 
+        asset = Asset(image_data=image_data)
+        db.session.add(asset)
+        db.session.commit()
+        img_url = asset.get_img_url()
+
+        new_comment = Comment(
+            netid=body.get("netid"),
+            comment=body.get("comment"),
+            park_id=park_id,
+            img_url=img_url,
+        )
+        db.session.add(new_comment)
+        db.session.commit()
+        return success_response(new_comment.serialize(),201)
+    else:
+        new_comment = Comment(
+        netid=body.get("netid"),
+        comment=body.get("comment"),
+        park_id=park_id,
+        )
+        db.session.add(new_comment)
+        db.session.commit()
+        return success_response(new_comment.serialize(),201)
+
+
+@app.route("/parks/<int:park_id>/comment/", methods=["GET"])
+def get_comment(park_id):
+    """
+    Endpoint for getting all comments for a park by id
+    """
+    park = Park.query.filter_by(id=park_id).first()
+    if not park:
+        return failure_response("park not found")
+
+    return success_response({"comments": [s.serialize() for s in park.comments]},201)
+
+
+
+@app.route("/comments/<int:comment_id>/", methods=["DELETE"])
+def delete_comment(comment_id):
+    """
+    Endpoint for deleting a comment by id
+    """
+    comment = Comment.query.filter_by(id=comment_id).first()
+    if not comment:
+        return failure_response("comment not found")
+    db.session.delete(comment)
+    db.session.commit()
+    return success_response(comment.serialize())
 
 
 
--- src/db.py
+++ src/db.py
@@ -1,11 +1,125 @@
 from unicodedata import category
 from flask_sqlalchemy import SQLAlchemy
-
+import base64
+import boto3
+import datetime
+import io
+from io import BytesIO
+from mimetypes import guess_extension, guess_type
+import os
+from PIL import Image
+import random
+import re
+import string
 
 db = SQLAlchemy()
 
+EXTENSIONS = ["png", "gif", "jpg","jpeg"]
+BASE_DIR = os.getcwd()
+S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
+S3_BASE_URL = f"https://{S3_BUCKET_NAME}.s3.us-east-1.amazonaws.com"
 
 
+####################
+# Image Class
+####################
+class Asset(db.Model):
+  """
+  Asset model
+  """
+  __tablename__ = "asset"
+  id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+  base_url = db.Column(db.String, nullable=True)
+  salt = db.Column(db.String, nullable=False)
+  extension = db.Column(db.Integer, nullable=False)
+  width = db.Column(db.Integer, nullable=False)
+  height = db.Column(db.Integer, nullable=False)
+  created_at = db.Column(db.DateTime, nullable=False)
+
+  def __init__(self, **kwargs):
+    """
+    Intialize an Asset object
+    """
+    self.create(kwargs.get("image_data"))
+
+  def serialize(self):
+    """
+    serialize an asset object
+    """
+    return {
+      "url": f"{self.base_url}/{self.salt}.{self.extension}",
+      "created_at": str(self.created_at)
+    }
+
+  def get_img_url(self):
+    """
+    return img url
+    """
+    return f"{self.base_url}/{self.salt}.{self.extension}"
+
+  def create(self, image_data):
+    """
+    given image in base64 form, check filetype, generate random string for image filename, decode image and upload it to AWS
+    """
+    try:
+      ext = guess_extension(guess_type(image_data)[0])[1:]
+      if ext not in EXTENSIONS:
+        raise Exception(f"unsupported file type: {ext}")
+
+      salt = "".join(
+        random.SystemRandom().choice(
+          string.ascii_uppercase + string.digits
+        )
+        for _ in range(16)
+      )
+
+      img_str = re.sub("^data:image/.+;base64,", "", image_data)
+      image_data = base64.b64decode(img_str)
+      img = Image.open(BytesIO(image_data))  #convert binary image data from base64 to Image object
+
+      self.base_url = S3_BASE_URL
+      self.salt = salt
+      self.extension = ext
+      self.width = img.width
+      self.height = img.height
+      self.created_at = datetime.datetime.now()
+
+      img_filename = f"{salt}.{ext}"
+      self.upload(img, img_filename)
+
+
+    except Exception as e:
+      print(f"errror when creating image: {e}")
+
+      
+  def upload(self, img, img_filename):
+    """
+    upload image to specified s3 bucket
+    """
+    try:
+      # save image temp on server
+      img_temploc = f"{BASE_DIR}/{img_filename}"
+      img.save(img_temploc)
+
+      # upload the image to S3
+      s3_client = boto3.client("s3")
+      s3_client.upload_file(img_temploc, S3_BUCKET_NAME, img_filename)
+
+      # make S3 image url public
+      s3_resource = boto3.resource("s3")
+      object_acl = s3_resource.ObjectAcl(S3_BUCKET_NAME, img_filename)
+      object_acl.put(ACL="public-read")
+
+      # remove imge from server
+      os.remove(img_temploc)
+
+    except Exception as e:
+      print(f"error when upolading image: {e}")
+
+
+####################
+# Park Class
+####################
 class Park(db.Model): 
     """
     parks model
@@ -14,14 +128,14 @@ class Park(db.Model):
     __tablename__ = "parks"    
     id = db.Column(db.Integer, primary_key=True)    
     name = db.Column(db.String, nullable=False)
-    latitude = db.Column(db.Float, nullable=False)
-    longitude = db.Column(db.Float, nullable=False)
+    latitude = db.Column(db.String, nullable=True)
+    longitude = db.Column(db.String, nullable=True)
     address = db.Column(db.String, nullable=False)
-    hourlyRate = db.Column(db.String, nullable=False)
-    dailyRate = db.Column(db.String, nullable=False)
-    rateDays = db.Column(db.String, nullable=False)
-    openHours = db.Column(db.String, nullable=False)
-    note = db.Column(db.String, nullable=False)
+    hourlyRate = db.Column(db.String, nullable=True)
+    dailyRate = db.Column(db.String, nullable=True)
+    rateDays = db.Column(db.String, nullable=True)
+    openHours = db.Column(db.String, nullable=True)
+    note = db.Column(db.String, nullable=True)
     comments = db.relationship("Comment", cascade="delete")
 
 
@@ -41,7 +155,37 @@ class Park(db.Model):
       self.comments = []
 
 
+    def serialize(self):  
+      """
+      convert object into json format
+      """
+      return {        
+          "id": self.id,  
+          "name": self.name,      
+          "latitude": self.latitude,        
+          "longitude": self.longitude,   
+          "address": self.address,   
+          "hourlyRate": self.hourlyRate,   
+          "dailyRate": self.dailyRate,   
+          "rateDays": self.rateDays,   
+          "openHours": self.openHours,   
+          "note": self.note,           
+          "comments":[s.serialize() for s in self.comments]
+          }
 
+    def simple_serialize(self):  
+      """
+      only provide minimal info about a park
+      """
+      return {        
+          "id": self.id,  
+          "name": self.name,
+          "address": self.address
+      } 
+
+####################
+# Comment Class
+####################
 class Comment(db.Model):
   """
   comment model
@@ -50,6 +194,9 @@ class Comment(db.Model):
   id = db.Column(db.Integer, primary_key=True)    
   netid = db.Column(db.String, nullable=False)
   comment = db.Column(db.String, nullable=False)
+  img_url = db.Column(db.String, nullable=True)
+  park_id = db.Column(db.Integer, db.ForeignKey("parks.id"), nullable=False)
+
 
   def __init__(self, **kwargs):
     """
@@ -57,4 +204,17 @@ class Comment(db.Model):
     """
     self.netid = kwargs.get("netid", "")
     self.comment = kwargs.get("comment", "")
+    self.park_id = kwargs.get("park_id")
+    self.img_url = kwargs.get("img_url","")
+    
 
+  def serialize(self):  
+    """
+    convert object into json format
+    """
+    return {        
+        "id": self.id,        
+        "netid": self.netid,     
+        "comment": self.comment,   
+        "img": self.img_url
+    }
